25e861fe440ee28d5464c2e3e5dd129b6765cbcd
Mahef23 <monkeygidr0@gmail.com>
2024-10-02 09:13:30 +0300

    Dialog age

---
+++ gross.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+        char name[30];
+        int age;
+        printf("Enter your Name");
+        scanf("%s",name);
+        printf("Enter your age");
+        scanf("%d",&age);
+
+        printf("-Hello %s \n Hello \n -How old are you \n I'm %d years old",name,age);
+
+    return 0;
+}
